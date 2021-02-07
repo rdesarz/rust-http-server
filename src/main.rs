d206e1fbd5b2556ddbf560191b26e97575f96fcf
Romain Desarzens <hib.pub@protonmail.com>
2021-02-07 13:31:45 +0100

    Load socket from command line

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use http_server::server::Server;
 use http_server::tcp_connection::TcpServerConnection;
+use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::str::FromStr;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let socket = &args[1];
+
     // Create connection for the server
-    let tcp_server_connection = TcpServerConnection::new(SocketAddr::new(
-        IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-        5666,
-    ))
+    let tcp_server_connection = TcpServerConnection::new(
+        SocketAddr::from_str(socket).expect("Specified socket does not exist"),
+    )
     .expect("Unable to initialize connection. Server shutdown");
     // Init Http server
     let http_server = Server::new(tcp_server_connection);
