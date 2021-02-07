1fd690c0e1a10e53fa2d03a27b417468b2bdbaf2
Romain Desarzens <hib.pub@protonmail.com>
2021-02-07 11:43:30 +0100

    Extract request handler in connection

--- src/tcp_connection.rs
+++ src/tcp_connection.rs
@@ -1,7 +1,7 @@
 use crate::server::Connection;
-use std::io;
 use std::io::{Read, Write};
-use std::net::{SocketAddr, TcpListener};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::{io, thread};
 
 pub struct TcpServerConnection {
     pub listener: TcpListener,
@@ -14,23 +14,32 @@ impl TcpServerConnection {
     }
 }
 
+impl TcpServerConnection {
+    fn handle_incoming_connection<T: Fn(&str) -> Result<String, std::io::Error>>(
+        request_handler_callback: T,
+        stream: &mut TcpStream,
+    ) {
+        let mut input_buffer: [u8; 1024] = [0; 1024];
+        stream.read(&mut input_buffer).unwrap();
+        match (request_handler_callback)(std::str::from_utf8(&input_buffer).unwrap()) {
+            Ok(message) => {
+                if let Ok(_written_size) = stream.write(message.as_bytes()) {
+                    stream.flush();
+                }
+            }
+            Err(e) => {
+                println!("Error when handling request: {:?}", e);
+            }
+        }
+    }
+}
+
 impl Connection for TcpServerConnection {
     fn listen<T: Fn(&str) -> Result<String, std::io::Error>>(&self, request_handler_callback: T) {
         for connection in self.listener.incoming() {
             match connection {
                 Ok(mut socket) => {
-                    let mut input_buffer: [u8; 1024] = [0; 1024];
-                    socket.read(&mut input_buffer).unwrap();
-                    match (request_handler_callback)(std::str::from_utf8(&input_buffer).unwrap()) {
-                        Ok(message) => {
-                            if let Ok(_written_size) = socket.write(message.as_bytes()) {
-                                socket.flush();
-                            }
-                        }
-                        Err(e) => {
-                            println!("Error when handling request: {:?}", e);
-                        }
-                    }
+                    Self::handle_incoming_connection(&request_handler_callback, &mut socket);
                 }
                 Err(e) => println!("Error when getting client: {:?}", e),
             }
